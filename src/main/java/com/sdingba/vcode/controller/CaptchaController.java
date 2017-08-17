fd78dcc7e44af00b6849b07271a4eae14c17d15f
shuai.xiong <15954809181@163.com>
2017-08-17 17:50:17 +0800

    fix

--- src/main/java/com/sdingba/vcode/controller/CaptchaController.java
+++ src/main/java/com/sdingba/vcode/controller/CaptchaController.java
@@ -6,31 +6,35 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.sdingba.vcode.config.VcodeConfig;
-import com.sdingba.vcode.server.CaptchaService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.Assert;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.collect.Maps;
+import com.sdingba.vcode.server.CaptchaService;
 
 @RestController
-@RequestMapping(value = "/v0.1/captcha")
+@RequestMapping(value = "/v0.1/vCodePlug")
 public class CaptchaController {
-    @Autowired
+
+    @Resource
     private CaptchaService captchaService;
 
     /**
      * 生成图片验证码
      */
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public void genCaptcha(HttpServletRequest req, HttpServletResponse resp) {
-        String token = req.getParameter("token");
-        // TODO:有效性检验优化
-        Assert.hasText(token);
-        captchaService.genCaptcha(token, resp);
+    public Map<String, Object> genCaptcha(HttpServletRequest req, HttpServletResponse resp) {
+        Map<String, Object> result = Maps.newHashMap();
+        String key = req.getParameter("token");
+        if (StringUtils.isNotEmpty(key)) {
+            captchaService.genCaptcha(key, resp);
+            result.put("result", true);
+        } else {
+            result.put("result", false);
+        }
+        return result;
     }
 
     /**
@@ -38,29 +42,21 @@ public class CaptchaController {
      */
     @RequestMapping(value = "/valid", method = RequestMethod.GET)
     public Map<String, Object> findCaptcha(HttpServletRequest req, HttpServletResponse resp) {
-        String token = req.getParameter("token");
+        String key = req.getParameter("token");
         String reqCaptchaCode = req.getParameter("captchaCode");
-        // TODO:有效性检验优化
-        Assert.hasText(token);
-        Assert.hasText(reqCaptchaCode);
-        String captchaCode = captchaService.findCaptcha(token);
-
-        Map<String, Object> params = Maps.newHashMap();
         boolean isValid = false;
-        if (reqCaptchaCode.equals(captchaCode)) {
-            isValid = true;
+        Map<String, Object> params = Maps.newHashMap();
+        if (StringUtils.isNotEmpty(key) && StringUtils.isNotEmpty(reqCaptchaCode)) {
+            String captchaCode = captchaService.findCaptcha(key);
+            if (reqCaptchaCode.equals(captchaCode)) {
+                isValid = true;
+            }
         }
         params.put("valid", isValid);
         return params;
     }
 
-    @Resource
-    private VcodeConfig vcodeConfig;
-
-    @RequestMapping(value = "a")
+    @RequestMapping(value = "test")
     public void test() {
-        System.out.println(vcodeConfig.getPassword() + " " + vcodeConfig.getHost());
-        System.out.println(vcodeConfig.getCacheType());
-        System.out.println(vcodeConfig.getPort());
     }
 }
